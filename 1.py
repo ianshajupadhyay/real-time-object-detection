e84fcdfda6276c24c1a04c81e589dbafc0579c95
ianshajupadhyay <31827458+ianshajupadhyay@users.noreply.github.com>
2018-09-20 00:48:48 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import new
+
+window = Tk()
+def r_t_o_d():
+	new.func()
+
+b1 = Button(window, text ="Real-time-object-detction",command=r_t_o_d)
+b1.grid(row =0 ,column=0 )
+
+
+window.mainloop()
+
